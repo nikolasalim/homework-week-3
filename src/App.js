a7e7c88d57c324ae64187c231f8ffdd3931563e5
nikolasalim <nikolasalim@gmail.com>
2020-02-14 22:28:59 +0100

    add API fetching and loading page (step 3)

--- src/App.js
+++ src/App.js
@@ -3,7 +3,11 @@ import "./App.css";
 import QuoteSearcher from "./components/QuoteSearcher";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <QuoteSearcher />
+    </div>
+  );
 }
 
 export default App;
--- src/components/QuoteSearcher.js
+++ src/components/QuoteSearcher.js
@@ -3,40 +3,39 @@ import Quote from "./Quote";
 
 export default class QuoteSearcher extends React.Component {
   state = {
-    quotes: [
-      {
-        _id: "5d91b45d9980192a317c8acc",
-        quoteText:
-          "Notice that the stiffest tree is most easily cracked, while the bamboo or willow survives by bending with the wind.",
-        quoteAuthor: "Bruce Lee"
-      },
-      {
-        _id: "5d91b45d9980192a317c8abe",
-        quoteText:
-          "Give me six hours to chop down a tree and I will spend the first four sharpening the axe.",
-        quoteAuthor: "Abraham Lincoln"
-      },
-      {
-        _id: "5d91b45d9980192a317c8955",
-        quoteText:
-          "Good timber does not grow with ease; the stronger the wind, the stronger the trees.",
-        quoteAuthor: "J. Willard Marriott"
-      }
-    ]
+    fetching: true,
+    quotes: []
   };
 
+  componentDidMount() {
+    fetch("https://quote-garden.herokuapp.com/quotes/search/tree")
+      .then(response => response.json())
+      .then(quote => {
+        this.setState({ fetching: false, quotes: quote.results });
+      });
+  }
+
   render() {
-    return (
-      <div>
-        <h1>Quotes</h1>
-        {this.state.quotes.map(quote => (
-          <Quote
-            key={quote._id}
-            quoteText={quote.quoteText}
-            quoteAuthor={quote.quoteAuthor}
-          ></Quote>
-        ))}
-      </div>
-    );
+    if (!this.state.fetching) {
+      return (
+        <div>
+          <h1>Quotes</h1>
+          {this.state.quotes.map(quote => (
+            <Quote
+              key={quote._id}
+              quoteText={quote.quoteText}
+              quoteAuthor={quote.quoteAuthor}
+            ></Quote>
+          ))}
+        </div>
+      );
+    } else {
+      return (
+        <div>
+          <h1>Quotes</h1>
+          <h4>LOADING...</h4>
+        </div>
+      );
+    }
   }
 }
