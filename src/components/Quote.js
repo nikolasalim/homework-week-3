ef158179610d2518ed202cd1478dc402667ea6d8
nikolasalim <nikolasalim@gmail.com>
2020-02-15 16:58:04 +0100

    work on search engine (step 6)

--- src/components/Quote.js
+++ src/components/Quote.js
@@ -2,28 +2,38 @@ import React from "react";
 
 export default class Quote extends React.Component {
   state = {
-    newStyle: null
+    // newStyle: [
+    //   { color: "green", fontWeight: "bold" },
+    //   { color: "red", fontWeight: "bold", textDecoration: "line-through" }
+    // ]
+    newStyle: null,
+    likeness: null
   };
 
   handleLike = () => {
-    this.props.addLike(this.props.id);
-    this.setState({ newStyle: { color: "green", fontWeight: "bold" } });
+    this.props.addLike(this.props.id, this.props.likeness);
+    this.setState({
+      newStyle: { color: "green", fontWeight: "bold" },
+      likeness: true
+    });
   };
 
   handleDislike = () => {
-    this.props.addDislike(this.props.id);
+    this.props.addDislike(this.props.id, this.props.likeness);
     this.setState({
       newStyle: {
         color: "red",
         fontWeight: "bold",
         textDecoration: "line-through"
-      }
+      },
+      likeness: false
     });
   };
 
   render() {
     return (
       <div>
+        <div></div>
         <p style={this.state.newStyle}>{this.props.quoteText}</p>
         <p>By: {this.props.quoteAuthor}</p>
         <button onClick={this.handleLike}>:)</button>
--- src/components/QuoteSearcher.js
+++ src/components/QuoteSearcher.js
@@ -3,27 +3,92 @@ import Quote from "./Quote";
 
 export default class QuoteSearcher extends React.Component {
   state = {
-    fetching: true,
-    quotes: []
+    fetching: null,
+    quotes: [],
+    likes: null,
+    dislikes: [],
+    search: ""
   };
 
   componentDidMount() {
-    fetch("https://quote-garden.herokuapp.com/quotes/search/tree")
+    fetch(
+      `https://quote-garden.herokuapp.com/quotes/search/${this.state.search}`
+    )
       .then(response => response.json())
       .then(quote => {
         this.setState({ fetching: false, quotes: quote.results });
       });
   }
 
-  //// maybe we'll use this?
-  addingLike = id => {};
-  addingDislike = id => {};
+  handleSearch = () => {
+    this.setState({ ...this.state, fetching: true });
+    this.componentDidMount();
+  };
+
+  handleChange = event => {
+    this.setState({ [event.target.name]: event.target.value });
+  };
+
+  addingLike = (id, like) => {
+    this.setState({ ...this.state, likes: this.state.likes + 1 });
+  };
+
+  addingDislike = id => {
+    // console.log("id is:", id);
+  };
 
   render() {
-    if (!this.state.fetching) {
+    if (this.state.fetching === null) {
       return (
         <div>
           <h1>Quotes</h1>
+
+          <div>
+            <form onSubmit={this.handleSearch}>
+              <label>
+                <input
+                  type="text"
+                  name="search"
+                  onChange={this.handleChange}
+                  value={this.state.search}
+                />
+              </label>
+              <input type="submit" value="Search" />
+            </form>
+          </div>
+
+          <h2>Liked:{this.state.likes}</h2>
+          <h2>Disliked:{this.state.dislikes}</h2>
+        </div>
+      );
+    } else if (this.state.fetching === true) {
+      return (
+        <div>
+          <h1>Quotes</h1>
+          <h4>LOADING...</h4>
+        </div>
+      );
+    } else if (this.state.fetching === false) {
+      return (
+        <div>
+          <h1>Quotes</h1>
+
+          <div>
+            <form onSubmit={this.handleSearch}>
+              <label>
+                <input
+                  type="text"
+                  name="search"
+                  onChange={this.handleChange}
+                  value={this.state.search}
+                />
+              </label>
+              <input type="submit" value="Search" />
+            </form>
+          </div>
+
+          <h2>Liked:{this.state.likes}</h2>
+          <h2>Disliked:{this.state.dislikes}</h2>
           {this.state.quotes.map(quote => (
             <Quote
               key={quote._id}
@@ -32,17 +97,40 @@ export default class QuoteSearcher extends React.Component {
               addLike={this.addingLike}
               addDislike={this.addingDislike}
               id={quote._id}
+              likeness={quote.likes}
             ></Quote>
           ))}
         </div>
       );
     } else {
-      return (
-        <div>
-          <h1>Quotes</h1>
-          <h4>LOADING...</h4>
-        </div>
-      );
+      return null;
     }
   }
 }
+
+// addingLike = id => {
+//   this.state.quotes.map(quote => {
+//     console.log(quote);
+//     if (quote._id === id) {
+//       if (!this.state.likes.includes(quote)) {
+//         console.log("is this single", quote);
+//         this.setState({ ...this.state, likes: this.state.likes.push(quote) });
+
+//         // return this.setState({
+//         //   ...this.state,
+//         //   likes: this.state.likes.push(quote)
+//         // });
+//         // console.log(this.state.likes.length);
+//       } else {
+//         return null;
+//       }
+//     } else {
+//       return null;
+//     }
+//   });
+//   console.log("likes arr:", this.state.likes);
+// };
+
+////////////////
+////////////////
+////////////////
